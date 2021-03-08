58c7b42613e77750979f0051c9971861b3f3cdaa
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-03-08 10:05:15 -0600

    Create quitaespaciosconswitchcase.c

---
+++ quitaespaciosconswitchcase.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <string.h> // strstr
+
+#define MAX 80
+#define TRUE 1
+#define FALSE 0
+
+int main() {
+  int c, number = FALSE, parenthesis = FALSE, quotes = FALSE;
+  while ((c = getchar()) != EOF) {
+      switch(c){
+      case ' ':
+          putchar('_');
+      default:
+        if(c!=' '){
+           putchar(c);
+        }
+      }
+
+
+
+  }
+  return 0;
+}
