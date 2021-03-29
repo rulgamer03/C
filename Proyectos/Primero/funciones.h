1ba1ec07ba1c16c584f3bbc464a6acc324382292
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-03-29 15:26:53 -0600

    Add files via upload

---
+++ Proyectos/Primero/funciones.h
@@ -0,0 +1,15 @@
+/* contiene declaraciones de variables y funciones*/
+#ifndef FUNCIONESH
+#define FUNCIONESH
+// FUNCIONESH
+#include <stdio.h>
+//Declaracion Funciones
+int suma(int a, int b);
+int resta(int a,int b);
+char primer_character(char* cadena);
+double area_circulo(int radio);
+
+//Declarar variables
+float PI;
+
+#endif
