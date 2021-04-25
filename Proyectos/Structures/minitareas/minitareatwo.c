be3cf49158e544d42dccae299c306471b2aa71ab
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-04-24 19:33:18 -0500

    Create minitareatwo.c

---
+++ Proyectos/Structures/minitareas/minitareatwo.c
@@ -0,0 +1,49 @@
+/*
+Provide command-line arguments (omitting the name of the executable that is implicitly considered to be the zero-position command-line argument) that will result in the program producing the printout 1, 4, 6.
+r -> 1 
+w -> 2 
+x -> 4 
+
+
+
+--------------
+r x wx
+1 4 6
+--------------
+*/
+
+
+#include <stdio.h>
+#define READ 01
+#define WRITE 02
+#define EXECUTE 04
+typedef enum {FALSE = 0, TRUE = 1} boolean;
+int main(int x, char* y[]) {
+  int i;
+  for (i = 1; i < x; i++) {
+    unsigned int status = 0;
+    char* pos = y[i];
+    boolean done = FALSE;
+    while (!done) {
+      switch (pos[0]) {
+      case '\0':
+        done = TRUE;
+        break;
+      case 'r':
+        status |= READ;
+        break;
+      case 'w':
+        status |= WRITE;
+        break;
+      case 'x':
+        status |= EXECUTE;
+        break;
+      default:
+        break;
+      }
+      pos++;
+    }
+    printf("%d ", status);
+  }
+  printf("\n");
+}
