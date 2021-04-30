69f8315205c151fb3fd03888d7f7e43edf54358a
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-04-29 20:46:21 -0500

    Create tree.h

---
+++ homework/mejoraenregistros/mejorahomework6/tree.h
@@ -0,0 +1,14 @@
+struct node {
+    int status;
+    char* user; //usuario 
+    char* password; //contraseña 
+    struct node *left, *right;
+};
+#define MAX 80
+char linea[MAX], password[MAX], user[MAX];
+void inorder(struct node* );
+void change(struct node* , char* , char* , int* );
+struct node* insert(struct node* , int , char* , char* );
+struct node* minValueNode(struct node* );
+struct node* deleteNode(struct node* , char* );
+void get (char* ,char* ,char* );
