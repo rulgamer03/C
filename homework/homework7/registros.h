997ec7e49a5dc9f4cfa9e3712d848ca1204702fd
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-05-04 17:12:59 -0500

    Create registros.h

---
+++ homework/homework7/registros.h
@@ -0,0 +1,10 @@
+#define MAX 20
+#define USUARIOS 27
+int* registrados = 0; //Variable global
+int** matrix; //matrix global
+int totaldesalidas=0; //Variable global
+int count();
+void registrar();
+void entradaosalida(int, int);
+void header();
+void horario();
