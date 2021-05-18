bb304d50a56d5b0a1f6af445817b48094e9d6940
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-05-18 08:10:27 -0500

    Add files via upload

---
+++ IntentosPractica/ExampleReversePolishCalculator/getop.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "calc.h"
+
+int getop(char s[])
+{
+    int i,c;
+
+    while((s[0] = c = getch()) == ' ' || c =='\t')
+        ;
+    s[1] = '\0';
+
+    i = 0;
+    if(!isdigit(c) && c!='.' && c!='-')
+        return c;
+
+    if(c=='-')
+        if(isdigit(c=getch()) || c == '.')
+            s[++i]=c;
+        else
+        {
+            if(c!=EOF)
+                ungetch(c);
+            return '-';
+        }
+
+    if(isdigit(c))
+        while(isdigit(s[++i] =c =getch()))
+            ;
+
+    if(c=='.')
+        while(isdigit(s[++i] = c=getch()))
+            ;
+
+    s[i] = '\0';
+    if(c!=EOF)
+        ungetch(c);
+    return NUMBER;
+}
