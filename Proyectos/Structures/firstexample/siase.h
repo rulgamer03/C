283b97fcc51fe15170117c03257eb85b1ac58450
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-05-18 08:15:36 -0500

    Delete Proyectos directory

--- Proyectos/Structures/firstexample/siase.h
+++
@@ -1,8 +0,0 @@
-struct estudiante { /// Data estudiante
-  unsigned int matr; /// Number for matricula 1234567, (0)987651
-  char* pe; /// Study program (3 letters)
-};
-
-typedef struct estudiante alumno; // Just say alumno as a nickname 
-
-void imprime(alumno); //print study information
--- Proyectos/Structures/secondexample/siase2.h
+++
@@ -1,9 +0,0 @@
-struct estudiante {
-  unsigned int matr;
-  char* pe;
-  unsigned int c; // Acces counter
-};
-
-typedef struct estudiante alumno;
-
-void imprime(alumno*); // it now uses a pointer in order to modify a variable 
