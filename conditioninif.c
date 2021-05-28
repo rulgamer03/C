d22f904e43fc24e8846183202ee99789014da82b
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-05-28 17:10:55 -0500

    Update conditioninif.c

--- conditioninif.c
+++ conditioninif.c
@@ -6,7 +6,7 @@ Write the <1> number:
 Number equal 100
 Write the <2> number:
 20
-Number less than 100
+Number smaller than 100
 Write the <3> number:
 800
 Number higher than 100
@@ -24,7 +24,7 @@ int main()
     for (i=0; i<n; i++){ //for condition
         printf ("\nWrite the <%d> number:\n", i+1);  //we add 1 to i , 0+1 -> 1 
         scanf("%d", &array[i]); // we ask for the number that we write in the position i, remember that we begin with 0
-        printf (array[i] == 100 ? "Number equal 100\n" : array[i] > 100 ? "Number higher than 100\n" : "Number less than 100\n"); // if is equal to 100 print "equal" else -> if is higher than 100 print "higher" else -> print "less"
+        printf (array[i] == 100 ? "Number equal 100\n" : array[i] > 100 ? "Number higher than 100\n" : "Number smaller than 100\n"); // if is equal to 100 print "equal" else -> if is higher than 100 print "higher" else -> print "less"
     }
 
     return 0;
