d795e5e214d3ac3e635762e63c5957858cf0129f
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-05-28 17:03:20 -0500

    Update conditioninif.c

--- conditioninif.c
+++ conditioninif.c
@@ -20,11 +20,11 @@ int main()
     int i;
     printf("How many numbers do you want?\n");
     scanf("%d", &n);
-    int array[n];
-    for (i=0; i<n; i++){
-        printf ("Write the <%d> number:\n", i+1);
-        scanf("%d", &array[i]);
-        printf (array[i] == 100 ? "Number equal 100\n" : array[i] > 100 ? "Number higher than 100\n" : "Number less than 100\n");
+    int array[n]; // array with n spaces
+    for (i=0; i<n; i++){ //for condition
+        printf ("Write the <%d> number:\n", i+1);  //we add 1 to i , 0+1 -> 1 
+        scanf("%d", &array[i]); // we ask for the number that we write in the position i, remember that we begin with 0
+        printf (array[i] == 100 ? "Number equal 100\n" : array[i] > 100 ? "Number higher than 100\n" : "Number less than 100\n"); // if is equal to 100 print "equal" else -> if is higher than 100 print "higher" else -> print "less"
     }
 
     return 0;
